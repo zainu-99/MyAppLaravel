5670115f1d1665bdc219f4e743c6511ccaf24f0e
zainu-99 <zainu.developer@gmail.com>
2020-12-21 13:34:03 +0700

    development

--- resources/views/appdashboard/masterdata/menu/option.blade.php
+++ resources/views/appdashboard/masterdata/menu/option.blade.php
@@ -1,5 +1,5 @@
 <?php $itemid = (isset($item_id)?$item_id:'') ?>
 <option @if($itemid == $child->id) selected @endif class="parent_option" id="parent_option_{{$child->id}}" value="{{$child->id}}">{!!$sparator.($sparator==''?'':'❯ ').$child->menu_text!!}</option>
 @foreach ($child->Menus as $parent)
-    @include('appdashboard.masterdata.menu.option', ['child' => $parent,'sparator'=>'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',"item_id"=>$item->menu_app_id]) 
+    @include('appdashboard.masterdata.menu.option', ['child' => $parent,'sparator'=>'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',"item_id"=>$itemid]) 
 @endforeach 
